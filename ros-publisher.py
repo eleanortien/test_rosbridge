b6a78afa47c7b1ed58ec7509bae69aa370cc6826
Eleanor <eleanortien@gmail.com>
2024-11-24 20:20:48 -0500

    revert ros-publisher to minimal

--- ros-publisher.py
+++ ros-publisher.py
@@ -4,25 +4,34 @@ from rclpy.node import Node
 from std_msgs.msg import String
 
 
-class TestPublisher(Node):
+class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, '/my_topic', 10)
+        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'Hello World: %d' % self.i
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    testpublisher = TestPublisher()
+    minimal_publisher = MinimalPublisher()
 
-    rclpy.spin(testpublisher)
-    msg = String()
-    msg.data = 'PM: 112.123'
-    testpublisher.publisher_.publish(msg)
+    rclpy.spin(minimal_publisher)
 
-
-    testpublisher.destroy_node()
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_publisher.destroy_node()
     rclpy.shutdown()
 
 
